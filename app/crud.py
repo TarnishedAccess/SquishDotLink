0d1a561f41266f1cf9a3be564a4122b062697215
Tarnished Access <110725925+TarnishedAccess@users.noreply.github.com>
2026-08-07 23:04:53 +0100

    added post and get. V1, barebones functional.

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from models import URL
-from utils import base62_encode
+from sqlalchemy import select
+from app.models import URL
+from app.utils import base62_encode
 
 async def create_url(db: AsyncSession, long_url: str) -> URL:
     new_url = URL(long_url=long_url)
@@ -12,3 +13,7 @@ async def create_url(db: AsyncSession, long_url: str) -> URL:
     await db.refresh(new_url)
 
     return new_url
+
+async def get_url_by_short_code(db: AsyncSession, short_code: str) -> URL:
+    result = await db.execute(select(URL).where(URL.short_code == short_code))
+    return result.scalars().first()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 import asyncio
-from database import get_db, async_session
-from crud import create_url
+from fastapi import FastAPI, Depends, HTTPException
+from app.database import get_db, async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.responses import RedirectResponse
+from app.schemas import URLCreate, URLResponse
+from app.crud import create_url, get_url_by_short_code
 
-async def main():
-    async with async_session() as session:
-        pass
+app = FastAPI()
 
-asyncio.run(main())
+@app.post("/urls", response_model=URLResponse)
+async def shorten_url(in_data: URLCreate, db: AsyncSession = Depends(get_db)):
+    new_url = await create_url(db, str(in_data.long_url))
+    return new_url
+
+@app.get("/{short_code}", response_model=URLResponse)
+async def get_url(short_code: str, db: AsyncSession = Depends(get_db)):
+    url = await get_url_by_short_code(db, short_code)
+    if url is None:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return RedirectResponse(url=url.long_url)
